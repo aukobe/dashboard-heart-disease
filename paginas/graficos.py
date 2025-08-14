69168b559f483ad9299331f6225f970717f073db
Augusto Koshiyama <100203154+aukobe@users.noreply.github.com>
2025-08-14 08:54:25 -0300

    Add files via upload

---
+++ paginas/graficos.py
@@ -0,0 +1,39 @@
+from ucimlrepo import fetch_ucirepo
+import plotly.express as px
+from dash import Dash, dcc, html
+import dash_bootstrap_components as dbc
+
+heart_disease = fetch_ucirepo(id = 45)
+dados = heart_disease.data.features
+dados['doenca'] = (heart_disease.data.targets > 0) * 1
+
+
+# Histograma idade
+figura_histograma = px.histogram(dados, x = 'age', title = 'Histograma de idades')
+div_do_histograma = html.Div([
+        dcc.Graph(figure = figura_histograma)
+    ])
+
+# Boxplot idade
+figura_boxplot = px.box(dados, x = 'doenca', y = 'age', color = 'doenca', title = 'Boxplot de idades')
+div_do_boxplot = html.Div([
+        dcc.Graph(figure = figura_boxplot)
+    ])
+
+# Boxplot colesterol
+
+figura_boxplot_chol = px.box(dados, x = 'doenca', y = 'chol', color = 'doenca', title = 'Boxplot de Colesterol Sérico')
+div_do_boxplot_chol = html.Div([
+    dcc.Graph(figure = figura_boxplot_chol)
+])
+
+layout = html.Div([
+    html.H1('Análise de dados do UCI Repository Heart Disease', className = 'text-center mb-5'),
+    dbc.Container([
+        dbc.Row([
+            dbc.Col([div_do_histograma], md = 6),
+            dbc.Col([div_do_boxplot], md = 3),
+            dbc.Col([div_do_boxplot_chol], md = 3)
+        ])
+    ])
+])
